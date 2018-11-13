0e5cbd87337e89311cc38d99393ae7c17dc3a0f6
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-11-12 19:34:52 -0800

    Add Providers to to render method to pass down store

--- client/app.js
+++ client/app.js
@@ -3,10 +3,19 @@
 // import third-party apps
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+
+import store from './redux/store';
 
 // import root app
 import App from './components/container/App';
 
 const MOUNT_NODE = document.getElementById('app');
 
-ReactDOM.render(<App />, MOUNT_NODE);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  MOUNT_NODE,
+);
