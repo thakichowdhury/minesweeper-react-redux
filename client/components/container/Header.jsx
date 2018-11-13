fe48cff492338dd03b2b16ef9b4c0d9761afe157
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-11-13 00:32:39 -0800

    Compose Header container component

---
+++ client/components/container/Header.jsx
@@ -0,0 +1,34 @@
+// @flow
+
+import { connect } from 'react-redux';
+
+import { gameStatus } from '../../redux/actions';
+
+import Message from '../presentational/Message';
+
+const { PLAYING, WIN, LOSE } = gameStatus;
+
+const parseMessage = (state) => {
+  const { status } = state;
+  if (state.status === PLAYING) {
+    return 'Minesweeper';
+  }
+  if (state.status === LOSE) {
+    return 'GAME OVER!';
+  }
+  if (state.status === WIN) {
+    return 'YOU WON!';
+  }
+  return state;
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  message: parseMessage(state),
+  status: state.status,
+});
+
+const VisibleBoard = connect(
+  mapStateToProps,
+)(Message);
+
+export default VisibleBoard;
