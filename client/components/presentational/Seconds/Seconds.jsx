38f4efd11dc3543f3f6d27b04dbc680de180d228
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-11-14 04:11:11 -0800

    Add styling and minute formatting to timer

--- client/components/presentational/Seconds/Seconds.jsx
+++ client/components/presentational/Seconds/Seconds.jsx
@@ -3,16 +3,23 @@
 import React, { type Node } from 'react';
 import store from '../../../redux/store';
 import { gameStatus, startTimer } from '../../../redux/actions';
+import styles from './Seconds.css';
 
 const Seconds = ({ status, seconds, isTicking }: { status: string, seconds: number, isTicking: boolean }): Node => {
   if (!isTicking && status === gameStatus.PLAYING) {
     startTimer()(store.dispatch);
   }
 
+  const minutes = Math.floor(seconds / 60);
+  let secondsFormat = seconds - minutes * 60 || 0;
+
+  if (secondsFormat < 10) {
+    secondsFormat = `0${secondsFormat}`;
+  }
+
   return (
-    <div>
-      {console.log(seconds)}
-      {seconds.toString()}
+    <div className={styles.container}>
+      <span className={styles.seconds}>{minutes}:{secondsFormat}</span>
     </div>
   );
 };
