766e86d22c67588cb0cf7081e8a94e3d1c8c9aa6
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-11-13 16:01:13 -0800

    Update flow  typechecking config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,4 @@
-// @flow
-
-type BabelConfigType = {
-  presets: Array<string>,
-  plugins: Array<string>
-};
-
-module.exports = (api: *): BabelConfigType => {
+module.exports = (api) => {
   api.cache(true);
   const presets = ['@babel/preset-env', '@babel/preset-react', 'airbnb'];
   const plugins = ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-flow-strip-types'];
